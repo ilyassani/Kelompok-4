b6d40a1287d5c38508a90e6892ccf4b436a9efec
Indah nabilla <53242619+IndahBillahr@users.noreply.github.com>
2019-07-30 19:38:16 -0700

    Add files via upload

---
+++ vms.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 31 Jul 2019 pada 02.19
+-- Versi Server: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jadwal`
+--
+
+CREATE TABLE `jadwal` (
+  `idJadwal` int(30) NOT NULL,
+  `kodePegawai` varchar(30) NOT NULL,
+  `verifikasi` int(30) NOT NULL,
+  `token` varchar(20) NOT NULL,
+  `waktu` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pegawai`
+--
+
+CREATE TABLE `pegawai` (
+  `kodePegawai` varchar(30) NOT NULL,
+  `namaPegawai` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `visitor`
+--
+
+CREATE TABLE `visitor` (
+  `company` int(70) NOT NULL,
+  `token` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `namaVisitor` int(50) NOT NULL,
+  `gender` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
